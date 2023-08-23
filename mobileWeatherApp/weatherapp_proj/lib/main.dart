3febb3763e636d2eac8aea27f5adc0111c9509ab
barimehdi77 <mbari@student.1337.ma>
2023-08-23 11:34:52 +0100

    feat: fix location in ios

--- mobileWeatherApp/weatherapp_proj/lib/main.dart
+++ mobileWeatherApp/weatherapp_proj/lib/main.dart
@@ -12,19 +12,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -189,7 +188,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           BottomNavigationBarItem(
             icon: Icon(
-              Icons.calendar_view_week,
+              Icons.calendar_month_outlined,
             ),
             label: 'Weekly',
           )
