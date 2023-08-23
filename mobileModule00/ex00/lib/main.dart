ef8db15d16956d4d31a7a7abcc8f01022f945f2b
barimehdi77 <mbari@student.1337.ma>
2023-08-23 10:50:09 +0100

    feat: add new subject changes

---
+++ mobileModule00/ex00/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text("A simple text"),
+            ElevatedButton(
+              onPressed: () {
+                debugPrint('Button Pressed');
+              },
+              child: const Text('Click me'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ mobileModule00/ex01/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String _text = 'A simple text';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(_text),
+            ElevatedButton(
+              onPressed: () {
+                debugPrint('Button Pressed');
+                if (_text == 'Hello World!') {
+                  setState(() {
+                    _text = 'A simple text';
+                  });
+                } else {
+                  setState(() {
+                    _text = 'Hello World!';
+                  });
+                }
+              },
+              child: const Text('Click me'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
